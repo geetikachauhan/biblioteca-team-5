bb8fd90bb134f6abda8bcc106e6f453b9a814f0c
rathipry <rathipry@gmail.com>
2020-12-14 20:13:07 +0530

    [Rathipriya]revert deletion of integration test file

---
+++ src/test/java/com/vapasi/biblioteca/controller/BookControllerITTest.java
@@ -0,0 +1,25 @@
+package com.vapasi.biblioteca.controller;
+
+import org.json.JSONException;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ExtendWith(SpringExtension.class)
+class BookControllerITTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    void shouldListTheBooks() throws JSONException {
+        String response = this.restTemplate.getForObject("/biblioteca/books/list" , String.class);
+        JSONAssert.assertEquals("[{id:1} , {id:2} ,{id:3} ,{id:4} ,{id:5} ,{id:6}]" , response , false);
+    }
+
+}
