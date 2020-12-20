5cc76bba819e37261505a832223a80c4a7652bc3
Sakthe Karthika <sakthe.karthika@gmail.com>
2020-12-20 09:20:47 +0530

    [Sakthe] Refactor data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,19 +1,19 @@
 insert into book(title, author, year_published, isbn, available)
-values ('A Game of Thrones', 'George R. R. Martin', 1996, '978-1-60309-025-4', true);
+values ('A Game of Thrones', 'George R. R. Martin', 1996, '978-1-60309-625-4', true);
 insert into book(title, author, year_published, isbn, available)
-values ('The Fellowship of the Ring', 'J. R. R. Tolkien', 1954, '978-1-60309-047-6', true);
+values ('The Fellowship of the Ring', 'J. R. R. Tolkien', 1954, '978-1-634309-047-6', true);
 insert into book(title, author, year_published, isbn, available)
-values ('The Lion, the Witch and the Wardrobe', 'C. S. Lewis', 1950, '978-1-60309-322-4', true);
+values ('The Lion, the Witch and the Wardrobe', 'C. S. Lewis', 1950, '978-1-603549-322-4', true);
 insert into book(title, author, year_published, isbn, available)
-values ('The Colour of Magic', 'Terry Pratchett', 1983, '978-1-891830-85-3', false);
+values ('The Colour of Magic', 'Terry Pratchett', 1983, '978-1-8918320-85-3', false);
 insert into book(title, author, year_published, isbn, available)
-values ('Assassin’s Apprentice', 'Robin Hobb', 1995, '978-1-60309-016-2', true);
+values ('Assassin’s Apprentice', 'Robin Hobb', 1995, '978-1-603129-016-2', true);
 insert into book(title, author, year_published, isbn, available)
-values ('The Lies of Locke Lamora', 'Scott Lynch', 2006, '978-1-60309-265-4', true);
+values ('The Lies of Locke Lamora', 'Scott Lynch', 2006, '978-1-603509-265-4', true);
 insert into book(title, author, year_published, isbn, available)
-values ('A Game of Thrones', 'George R. R. Martin', 1996, '978-1-60309-234-4', true);
+values ('A Game of Thrones', 'George R. R. Martin', 1996, '978-1-603349-234-4', true);
 insert into book(title, author, year_published, isbn, available)
-values ('The Fellowship of the Ring', 'J. R. R. Tolkien', 1954, '978-1-60309-321-6', true);
+values ('The Fellowship of the Ring', 'J. R. R. Tolkien', 1954, '978-1-603109-321-6', true);
 
 insert into movie(movie_name, movie_year, director, rating, available)
 values ('Harry Potter and the Philosopher’s Stone', 2001, 'Chris Columbus', 7.6, true);
